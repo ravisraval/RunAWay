3f48543ba82fcd0ed183b08b44387034adc52b84
Ravi <raviraval@gmail.com>
2017-07-26 10:18:30 -0700

    Parse params in controller to create duration values

--- app/controllers/api/workouts_controller.rb
+++ app/controllers/api/workouts_controller.rb
@@ -2,7 +2,7 @@ class Api::WorkoutsController < ApplicationController
   before_action :require_signed_in
 
   def create
-    @workout = Workout.new(workout_params)
+    @workout = Workout.new(parsed_params)
     @workout.user_id = current_user.id
 
     if @workout.save
@@ -32,7 +32,26 @@ class Api::WorkoutsController < ApplicationController
   end
 
   private
+
   def workout_params
-    params.require(:workout).permit(:title, :route_id, :notes, :distance, :elevation_change, :biked, :date_time, :duration_seconds)
+    params.require(:workout).permit(:title, :route_id, :notes, :distance, :elevation_change, :biked, :date_time, :hours, :minutes, :seconds)
+  end
+
+  def parsed_params
+    params = workout_params.except(:hours, :minutes, :seconds)
+
+    hours = workout_params[:hours].to_i
+    minutes = workout_params[:minutes].to_i
+    seconds = workout_params[:seconds].to_i
+
+    params[:duration_seconds] = (( hours * 60 ) + minutes ) * 60 + seconds
+
+    msep, ssep = "", ""
+    msep = "0" if minutes < 10
+    ssep = "0" if seconds < 10
+
+    params[:duration_string] = "#{hours}:#{msep}#{minutes}:#{ssep}#{seconds}"
+
+    params
   end
 end
