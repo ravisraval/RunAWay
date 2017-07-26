8903f2eba342496d054756bbb660b5690cadd8d3
Ravi <raviraval@gmail.com>
2017-07-26 10:27:26 -0700

    Pass new params through view builder

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -4,21 +4,6 @@ class Workout < ApplicationRecord
                         numericality: { greater_than: 0,
                                         allow_nil: true  }
   belongs_to :user
-  belongs_to :route
-
-  def duration
-    hsep = ""
-    msep = ""
-    ssep = ""
-    sec = self.duration_seconds
-    hours = sec / 3600
-    sec -= hours * 3600
-    minutes = sec / 60
-    sec -= minutes * 60
-    hsep = "0" if hours < 10
-    msep = "0" if minutes < 10
-    ssep = "0" if sec < 10
-    "#{hsep}#{hours}:#{msep}#{minutes}:#{ssep}#{sec}"
-  end
+  belongs_to :route, optional: true
 
 end
--- app/views/api/workouts/_workout.json.jbuilder
+++ app/views/api/workouts/_workout.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! workout, :id, :user_id, :route_id, :notes, :elevation_change, :biked, :date_time, :duration, :title, :distance
+json.extract! workout, :id, :user_id, :route_id, :notes, :elevation_change, :biked, :date_time, :duration_seconds, :duration_string, :title, :distance
