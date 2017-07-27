abad861a1acb2cef61605dbf89ee79365e51576d
Ravi <raviraval@gmail.com>
2017-07-26 20:20:06 -0700

    heroku1

--- frontend/components/workout/workout_show.jsx
+++ frontend/components/workout/workout_show.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import { Link, withRouter } from 'react-router-dom';
 
 
 // const BenchShow = ({ workout, benchId, fetchBench }) => {
