79cbcff84a0d44b7ecc73db03cf12f522c950da9
Ravi <raviraval@gmail.com>
2017-07-27 09:39:11 -0700

    Start stats

--- frontend/components/stats/stats.jsx
+++ frontend/components/stats/stats.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
 
 class Stats extends React.Component {
   constructor() {
@@ -6,17 +8,40 @@ class Stats extends React.Component {
 
   }
 
+  componentDidMount() {
+    this.props.fetchWorkouts();
+  }
+
   render() {
+    const {workouts} = this.props;
 
     return(
       <div className="stats-page">
-        <h1>"{this.props.username}' statistics"</h1>
-        <ul>
-          <li>
-            
-          </li>
-        </ul>
-
+        <h1>{this.props.username}s statistics</h1>
+        <div className="personal-stats">
+          <h2>Personal Stats</h2>
+          <div className="personal-stats-list">
+            <h3>Combined Cycling & Running Totals</h3>
+              <span>Workouts: </span>
+              <span>Distance Traveled: </span>
+              <span>Duration: </span>
+            <h3>Cycling Totals</h3>
+              <span>Workouts: </span>
+              <span>Distance Traveled: </span>
+              <span>Duration: </span>
+              <span>Longest ride:</span>
+              <span>Farthest ride:</span>
+            <h3>Running Totals</h3>
+              <span>Workouts: </span>
+              <span>Distance Traveled: </span>
+              <span>Duration: </span>
+              <span>Longest run:</span>
+              <span>Farthest ride:</span>
+          </div>
+        </div>
+        <div className="global-stats">
+          <h2>Global Stats</h2>
+        </div>
       </div>
     );
   }
--- frontend/components/stats/stats_container.js
+++ frontend/components/stats/stats_container.js
@@ -1,8 +1,18 @@
 import { connect } from 'react-redux';
 import Stats from './stats';
+import { fetchWorkouts } from '../../actions/workout_actions';
+import { allWorkouts } from '../../reducers/selectors';
 
 const mapStateToProps = state => ({
-  username: state.session.currentUser.username
+  username: state.session.currentUser.username,
+  workouts: allWorkouts(state)
 });
 
-export default connect(mapStateToProps, null)(Stats);
+const mapDispatchToProps = dispatch => ({
+  fetchWorkouts: () => dispatch(fetchWorkouts())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Stats);
